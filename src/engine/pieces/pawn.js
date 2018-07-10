8da82a97e17dd8ea151b0d251e0cd247f42d097a
Ash Brent-Carpenter <ash@sent.com>
2018-07-10 09:57:01 +0100

    let pawns move 2 squares

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,8 +9,16 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
     	const boardLocation = board.findPiece(this);
-    	return [Square.at(this.player === Player.WHITE
+    	const isWhite = this.player === Player.WHITE;
+    	let moves = [];
+    	moves.push(Square.at(isWhite
     		? boardLocation.row + 1
-    		: boardLocation.row - 1, boardLocation.col)];
+    		: boardLocation.row - 1, boardLocation.col));
+    	if (boardLocation.row === (isWhite ? 1 : 6)) {
+    		moves.push(Square.at(isWhite
+    			? boardLocation.row + 2
+    			: boardLocation.row - 2, boardLocation.col));
+    	}
+    	return moves;
     }
 }
