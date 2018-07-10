0a99632d55e2d8a76468c82c6ab3948c8dd1095e
Ash Brent-Carpenter <ash@sent.com>
2018-07-10 11:50:12 +0100

    pawns cannot move if pieces in the way

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,10 +11,17 @@ export default class Pawn extends Piece {
     	const boardLocation = board.findPiece(this);
     	const direction = this.player === Player.WHITE ? 1 : -1;
     	let moves = [];
-    	moves.push(Square.at(boardLocation.row + direction, boardLocation.col));
-    	if (!this.hasMoved) {
-    		moves.push(Square.at(boardLocation.row + 2 * direction, boardLocation.col));
-    	}
+
+        const squareAhead = Square.at(boardLocation.row + direction, boardLocation.col);
+        const squareTwoAhead = Square.at(boardLocation.row + 2 * direction, boardLocation.col);
+
+        if (!board.getPiece(squareAhead)) {
+            moves.push(squareAhead);
+            if (!this.hasMoved && !board.getPiece(squareTwoAhead)) {
+                moves.push(squareTwoAhead);
+            }
+        }
+
     	return moves;
     }
 }
