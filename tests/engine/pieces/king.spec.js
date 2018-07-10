6af517ccd9cfee9ac33965a44e7af55c8406864a
Ash Brent-Carpenter <ash@sent.com>
2018-07-10 13:57:27 +0100

    add tests for king and rook

--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -3,6 +3,7 @@ import King from '../../../src/engine/pieces/king';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
+import Pawn from '../../../src/engine/pieces/pawn'
 
 describe('King', () => {
 
@@ -41,5 +42,50 @@ describe('King', () => {
         const expectedMoves = [Square.at(0, 1), Square.at(1, 1), Square.at(1, 0)];
 
         moves.should.deep.have.members(expectedMoves);
+    });
+
+    it('can take opposing pieces laterally', () => {
+        const king = new King(Player.WHITE);
+        const opposingPiece = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(4, 5), opposingPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.deep.include(Square.at(4, 5));
+    });
+
+    it('cannot take friendly pieces laterally', () => {
+        const king = new King(Player.WHITE);
+        const otherPiece = new Pawn(Player.WHITE);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(4, 5), otherPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(4, 5));
+    });
+
+
+    it('can take opposing pieces diagonally', () => {
+        const king = new King(Player.WHITE);
+        const opposingPiece = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(5, 5), opposingPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.deep.include(Square.at(5, 5));
+    });
+
+    it('cannot take friendly pieces diagonally', () => {
+        const king = new King(Player.WHITE);
+        const otherPiece = new Pawn(Player.WHITE);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(5, 5), otherPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(5, 5));
     });
 });
