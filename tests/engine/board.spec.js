88bd770629b950a3123c2349de86566eef28c94d
Ash Brent-Carpenter <ash@sent.com>
2018-07-11 14:03:58 +0100

    add tests for detecting check

--- tests/engine/board.spec.js
+++ tests/engine/board.spec.js
@@ -1,6 +1,8 @@
 import 'chai/register-should';
 import Board from '../../src/engine/board';
 import Pawn from '../../src/engine/pieces/pawn';
+import King from '../../src/engine/pieces/king';
+import Queen from '../../src/engine/pieces/queen';
 import Player from '../../src/engine/player';
 import Square from '../../src/engine/square';
 
@@ -37,5 +39,32 @@ describe('Board', () => {
             board.findPiece(pawn).should.eql(square); // Object equivalence: different objects, same data
         });
 
+    });
+
+    describe('check', () => {
+        let board;
+        beforeEach(() => { // Common code executed before each test.
+            board = new Board();
+        });
+
+        it('is recognised', () => {
+            const king = new King(Player.WHITE);
+            const queen = new Queen(Player.BLACK);
+
+            board.setPiece(Square.at(3, 3), king);
+            board.setPiece(Square.at(3, 4), queen);
+
+            board.isCheck(Player.WHITE).should.be.true();
+        });
+        
+        it('is not incorrectly stated', () => {
+            const king = new King(Player.WHITE);
+            const queen = new Queen(Player.BLACK);
+
+            board.setPiece(Square.at(3, 3), king);
+            board.setPiece(Square.at(4, 5), queen);
+
+            board.isCheck(Player.WHITE).should.be.false();
+        });
     });
 });
