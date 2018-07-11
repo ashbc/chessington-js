e818b443115ecfeb169b4e2158cd714574db2a86
Ash Brent-Carpenter <ash@sent.com>
2018-07-11 15:08:45 +0100

    add tests for moving and and out of check

--- tests/engine/board.spec.js
+++ tests/engine/board.spec.js
@@ -2,6 +2,7 @@ import 'chai/register-should';
 import Board from '../../src/engine/board';
 import Pawn from '../../src/engine/pieces/pawn';
 import King from '../../src/engine/pieces/king';
+import Rook from '../../src/engine/pieces/rook';
 import Queen from '../../src/engine/pieces/queen';
 import Player from '../../src/engine/player';
 import Square from '../../src/engine/square';
@@ -65,6 +66,86 @@ describe('Board', () => {
             board.setPiece(Square.at(4, 5), queen);
 
             board.isCheck(Player.WHITE).should.be.false;
+        });
+
+        describe('moving into check', () => {
+            it('king cannot move to a threatened square', () => {
+                const king = new King(Player.WHITE);
+                const rook = new Rook(Player.BLACK);
+
+                board.setPiece(Square.at(0, 0), king);
+                board.setPiece(Square.at(7, 1), rook);
+
+                const moves = king.getAvailableMoves(board);
+
+                moves.should.not.deep.include(Square.at(1, 1));
+                moves.should.not.deep.include(Square.at(0, 1));
+            });
+            
+            it('pieces cannot stop blocking the king', () => {
+                const king = new King(Player.WHITE);
+                const rook = new Rook(Player.BLACK);
+                const pawn = new Pawn(Player.WHITE);
+
+                board.setPiece(Square.at(1, 0), king);
+                board.setPiece(Square.at(1, 2), pawn);
+                board.setPiece(Square.at(1, 5), rook);
+
+                const moves = pawn.getAvailableMoves(board);
+                moves.should.not.deep.include(Square.at(2, 2));
+            });
+        });
+
+        describe('leaving check', () => {
+            it('players cannot remain in check', () => {
+                const king = new King(Player.WHITE);
+                const rook = new Rook(Player.BLACK);
+                const pawn = new Pawn(Player.WHITE);
+
+                board.setPiece(Square.at(0, 0), king);
+                board.setPiece(Square.at(0, 1), rook);
+                board.setPiece(Square.at(5, 7), pawn);
+
+                const moves = pawn.getAvailableMoves(board);
+
+                moves.should.be.empty;
+            });
+
+            it('players can leave check by blocking', () => {
+                const king = new King(Player.WHITE);
+                const rook = new Rook(Player.BLACK);
+                const queen = new Queen(Player.WHITE);
+
+                board.setPiece(Square.at(0, 0), king);
+                board.setPiece(Square.at(0, 5), rook);
+                board.setPiece(Square.at(2, 2), queen);
+
+                const moves = queen.getAvailableMoves(board);
+
+                moves.should.deep.include(Square.at(0, 2));
+            });
+
+            it('players can leave check by moving the king', () => {
+                const king = new King(Player.WHITE);
+                const rook = new Rook(Player.BLACK);
+                board.setPiece(Square.at(0, 0), king);
+                board.setPiece(Square.at(0, 1), rook);
+
+                const moves = king.getAvailableMoves(board);
+
+                moves.should.deep.include(Square.at(1, 0));
+            });
+
+            it('players can leave check by taking', () => {
+                const king = new King(Player.WHITE);
+                const rook = new Rook(Player.BLACK);
+                board.setPiece(Square.at(0, 0), king);
+                board.setPiece(Square.at(0, 1), rook);
+
+                const moves = king.getAvailableMoves(board);
+
+                moves.should.deep.include(Square.at(0, 1));
+            });
         });
     });
 });
